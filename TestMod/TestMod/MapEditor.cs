a12bf0f723409d7cef989f5b302e68bf41605759
Fox536 <ultimate_X_25@yahoo.com>
2017-12-05 13:24:28 -0600

    0.1

---
+++ TestMod/TestMod/MapEditor.cs
@@ -0,0 +1,235 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xTile.Tiles;
+
+namespace TestMod.MapEditor
+{
+	public static class TreeArea
+	{
+		/*	Tile(int layer, int x, int y, int tileIndex)
+			Layer
+				0: Back
+				1: Buildings
+				2: Paths
+				3: Front
+				4: AlwaysFront */
+
+		public static void AddCenter(List<Tile> tileList, int startX, int startY)
+		{
+			// Back Grassy Edits
+			tileList.Add(new Tile(0, startX, startY, 200));
+			tileList.Add(new Tile(0, startX + 1, startY, 201));
+			tileList.Add(new Tile(0, startX + 2, startY, 203));
+
+			tileList.Add(new Tile(0, startX, startY + 1, 225));
+			tileList.Add(new Tile(0, startX + 1, startY + 1, 206));
+			tileList.Add(new Tile(0, startX + 2, startY + 1, 228));
+
+			tileList.Add(new Tile(0, startX, startY + 2, 250));
+			tileList.Add(new Tile(0, startX + 1, startY + 2, 251));
+			tileList.Add(new Tile(0, startX + 2, startY + 2, 253));
+		}
+
+		// Sidding
+		public static void AddLeftSide(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 228));
+		}
+		public static void AddRightSide(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 225));
+		}
+		public static void AddTopSide(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 251));
+		}
+		public static void AddBottomSide(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 201));
+		}
+
+		// Corners
+		public static void AddCornerTopLeft(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 178));
+		}
+		public static void AddCornerTopRight(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 252));
+		}
+		public static void AddCornerBottomLeft(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 177));
+		}
+		public static void AddCornerBottomRight(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 202));
+		}
+
+
+		public static void AddTurnLeftBottom(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 253));
+		}
+		
+
+		public static void AddGrass(List<Tile> tileList, int startX, int startY)
+		{
+			tileList.Add(new Tile(0, startX, startY, 175));
+		}
+
+		public static void AddNoSpawn(List<Tile> tileList, List<int[]> noSpawnArea, GameLocation gl, int startX, int startY)
+		{
+			//tileList.Add(new Tile(3, startX, startY, 232));
+			noSpawnArea.Add(new int[] { startX, startY });
+			//gl.setTileProperty(startX, startY, "Back", "NoSpawn", "All");
+		}
+
+		public static void FillSquareArea(int startX, int startY, int width, int height, bool left, bool right, bool top, bool bottom, bool topLeft, bool topRight, bool bottomLeft, bool bottomRight)
+		{
+			// Create Arrays
+			List<Tile> tileEdits = new List<Tile>();
+
+			List<int[]> noSpawnArea = new List<int[]>();
+
+
+			//--------------------------------------------------------------------------
+
+			//--------------------------------------------------------------------------
+			// Main Area
+			//--------------------------------------------------------------------------
+			// Modding - Add TileData Changes
+			for (int x = 0; x < width * 3; x++)
+			{
+				for (int y = 0; y < height * 3; y++)
+				{
+					AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX + x, startY + y);
+				}
+			}
+
+			// Modding - Add TileId Changes
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					AddCenter(tileEdits, startX + x * 3, startY + y * 3);
+				}
+			}
+			//--------------------------------------------------------------------------
+
+			//--------------------------------------------------------------------------
+			// Sidding
+			//--------------------------------------------------------------------------
+			// Modding - Add Left Sidding
+			if (left)
+			{
+				for (int y = 0; y < height * 3; y++)
+				{
+					AddLeftSide(tileEdits, startX - 1, startY + y);
+					AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX - 1, startY + y);
+				}
+			}
+			// Modding - Add Right Sidding
+			if (right)
+			{
+				int rightSide = startX + width * 3;
+				for (int y = 0; y < height * 3; y++)
+				{
+					AddRightSide(tileEdits, rightSide, startY + y);
+					AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], rightSide, startY + y);
+				}
+			}
+			// Modding - Add Top Sidding
+			if (top)
+			{
+				for (int x = 0; x < width * 3; x++)
+				{
+					AddTopSide(tileEdits, startX + x, startY - 1);
+					AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX + x, startY - 1);
+				}
+			}
+			// Modding - Add Bottom Sidding
+			if (bottom)
+			{
+				int bottomSide = startY + height * 3;
+				for (int x = 0; x < width * 3; x++)
+				{
+					AddBottomSide(tileEdits, startX + x, bottomSide);
+					AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX + x, bottomSide);
+				}
+			}
+			//--------------------------------------------------------------------------
+
+			//--------------------------------------------------------------------------
+			// Corners
+			//--------------------------------------------------------------------------
+			// Modding - Add TopLeft Sidding
+			if (topLeft)
+			{
+				AddCornerTopLeft(tileEdits, startX - 1, startY - 1);
+				AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX - 1, startY - 1);
+			}
+			// Modding - Add TopRight Sidding
+			if (topRight)
+			{
+				int rightSide = startX + width * 3;
+				AddCornerTopRight(tileEdits, rightSide, startY - 1);
+				AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], rightSide, startY - 1);
+			}
+			// Modding - Add BottomLeft Sidding
+			if (bottomLeft)
+			{
+				int bottomSide = startY + height * 3;
+				AddCornerBottomLeft(tileEdits, startX - 1, bottomSide);
+				AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], startX - 1, bottomSide);
+			}
+			// Modding - Add BottomRight Sidding
+			if (bottomRight)
+			{
+				int rightSide = startX + width * 3;
+				int bottomSide = startY + height * 3;
+				AddCornerBottomRight(tileEdits, rightSide, bottomSide);
+				AddNoSpawn(tileEdits, noSpawnArea, Game1.locations[1], rightSide, bottomSide);
+			}
+			//--------------------------------------------------------------------------
+			
+			// Finalize Edits
+			PatchMap(Game1.locations[1], tileEdits);
+
+			for (int i = 0; i < noSpawnArea.Count; i++)
+			{
+				int x = noSpawnArea[i][0];
+				int y = noSpawnArea[i][1];
+				Game1.locations[1].setTileProperty(x, y, "AlwaysFront", "NoSpawn", "All");
+			}
+		}
+
+		public static void PatchMap(GameLocation gl, List<Tile> tileArray)
+		{
+			foreach (Tile tile in tileArray)
+			{
+				if (tile.tileIndex < 0)
+				{
+					gl.removeTile(tile.x, tile.y, tile.layer);
+					continue;
+				}
+
+				if (gl.map.Layers[tile.l].Tiles[tile.x, tile.y] == null)
+				{
+					gl.map.Layers[tile.l].Tiles[tile.x, tile.y] = new StaticTile(gl.map.GetLayer(tile.layer), gl.map.TileSheets[gl.map.TileSheets.Count-1], xTile.Tiles.BlendMode.Alpha, tile.tileIndex);
+				}
+				else
+				{
+					gl.setMapTileIndex(tile.x, tile.y, tile.tileIndex, tile.layer);
+				}
+			}
+		}
+
+		
+	}
+
+}
---
+++ TestMod/TestMod/ModEntry.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using xTile.Tiles;
+
+namespace TestMod
+{
+	/// <summary>The mod entry point.</summary>
+	public class ModEntry : Mod
+	{
+		private static List<Tile> FarmEdit1 = new List<Tile>()
+		{
+
+		};
+
+		//Meticulously planned tile edits for Farm.xnb
+		/*Create array of custom tile objects with layer, x & y location, and new texture information
+            (-1 as a texture means set the tile to null) */
+		private static List<Tile> FarmEdits = new List<Tile>()
+		{
+			new Tile(0, 0, 38, 175), new Tile(0, 1, 38, 175), new Tile(0, 0, 43, 537),
+			new Tile(0, 1, 43, 537), new Tile(0, 2, 43, 586), new Tile(0, 0, 44, 566),
+			new Tile(0, 1, 44, 537), new Tile(0, 2, 44, 618), new Tile(0, 0, 45, 587),
+			new Tile(0, 1, 45, 473), new Tile(0, 0, 46, 587), new Tile(0, 1, 46, 587),
+			new Tile(0, 0, 48, 175), new Tile(0, 1, 48, 175),
+
+			new Tile(1, 0, 39, 175), new Tile(1, 1, 39, 175), new Tile(1, 2, 39, 444),
+			new Tile(1, 0, 40, 446), new Tile(1, 1, 40, 468), new Tile(1, 2, 40, 469),
+			new Tile(1, 0, 41, 492), new Tile(1, 1, 41, 493), new Tile(1, 2, 41, 494),
+			new Tile(1, 0, 42, 517), new Tile(1, 1, 42, 518), new Tile(1, 2, 42, 519),
+			new Tile(1, 0, 43, 542), new Tile(1, 1, 43, 543), new Tile(1, 2, 43, 544),
+			new Tile(1, 0, 44, -1), new Tile(1, 1, 44, -1), new Tile(1, 2, 44, -1),
+			new Tile(1, 0, 45, -1), new Tile(1, 1, 45, -1), new Tile(1, 2, 45, -1),
+			new Tile(1, 0, 46, -1), new Tile(1, 1, 46, -1), new Tile(1, 2, 46, -1),
+			new Tile(1, 0, 47, 175), new Tile(1, 1, 47, 175), new Tile(1, 0, 48, -1),
+
+			new Tile(3, 0, 36, -1), new Tile(3, 1, 36, -1), new Tile(3, 0, 37, -1),
+			new Tile(3, 1, 37, -1), new Tile(3, 0, 38, -1), new Tile(3, 1, 38, -1),
+			new Tile(3, 0, 39, -1), new Tile(3, 1, 39, -1), new Tile(3, 0, 40, -1),
+			new Tile(3, 0, 41, -1), new Tile(3, 0, 46, 414), new Tile(3, 1, 46, 413),
+			new Tile(3, 2, 46, 438), new Tile(3, 0, 47, 175), new Tile(3, 1, 47, 175),
+			new Tile(3, 2, 47, 394)
+		};
+
+
+		/*********
+        ** Public methods
+        *********/
+		/// <summary>The mod entry point, called after the mod is first loaded.</summary>
+		/// <param name="helper">Provides simplified APIs for writing mods.</param>
+		public override void Entry(IModHelper helper)
+		{
+
+
+			//or if you've created your own GameLocation object when you loaded your mod, just send that instead of referencing the Game1.locations array
+			/*
+            //These are random made up examples
+            Game1.locations[1].warps.Add(new Warp(64, 19, "Farm", 62, 23, false));
+            Game1.locations[1].setTileProperty(63, 13, "Buildings", "Action", "Warp 64 18 Farm");
+            */
+
+			SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+
+			//ControlEvents.KeyPressed += this.ControlEvents_KeyPress;
+		}
+
+
+
+		/*********
+        ** Private methods
+        *********/
+		/// <summary>The method invoked when the player presses a keyboard button.</summary>
+		/// <param name="sender">The event sender.</param>
+		/// <param name="e">The event data.</param>
+		private void ControlEvents_KeyPress(object sender, EventArgsKeyPressed e)
+		{
+			if (Context.IsWorldReady) // save is loaded
+			{
+				this.Monitor.Log($"{Game1.player.name} pressed {e.KeyPressed}.");
+			}
+		}
+
+		private void SaveEvents_AfterLoad(object sender, EventArgs e)
+		{
+			this.Monitor.Log($"Tilesheet Count: {Game1.locations[1].map.TileSheets.Count}");
+
+
+			//this.Monitor.Log($"{FarmEdits.Count}");
+			this.Monitor.Log($"Adding Map Edits");
+
+			this.Monitor.Log($"Adding Sector 1");
+			AddSector1();
+			this.Monitor.Log($"Adding Sector 2");
+			AddSector2();
+			this.Monitor.Log($"Adding Sector 3");
+			AddSector3();
+			this.Monitor.Log($"Adding Sector 4");
+			AddSector4();
+
+
+			this.Monitor.Log($"Finished Adding Sectors");
+			//MapEditor.TreeArea.PatchMap(Game1.locations[1], tileEdits);
+		}
+
+		private static void AddSector1()
+		{
+			int startX = 69;
+			int startY = 74;
+			int width = 3;
+			int height = 14;
+
+			MapEditor.TreeArea.FillSquareArea(startX, startY, width, height, true, false, false, false, false, false, false, false);
+
+			// Custom Fix Needed
+			// Create Array
+			List<Tile> tileEdits = new List<Tile>();
+
+			// Fix Right Side
+			int rightSide = startX + width * 3;
+			for (int y = 0; y < height; y++)
+			{
+				MapEditor.TreeArea.AddGrass(tileEdits, rightSide, startY + y);
+			}
+			// Add Left & Bottom Turn
+			MapEditor.TreeArea.AddTurnLeftBottom(tileEdits, 68, 115);
+
+			// Finalize Edits
+			MapEditor.TreeArea.PatchMap(Game1.locations[1], tileEdits);
+
+			return;
+			/*
+			// Create Array
+			List<Tile> tileEdits = new List<Tile>();
+
+			// Modding - Start Point
+			int startX = 69;
+			int startY = 74;
+			
+			// Modding - Add TileData Changes
+			for (int x = 0; x < 3 * 3; x++)
+			{
+				for (int y = 0; y < 14 * 3; y++)
+				{
+					//squares.Add(new int[] { startX + x, startY + y});
+					MapEditor.TreeArea.AddNoSpawn(tileEdits, Game1.locations[1], startX + x, startY + y);
+				}
+			}
+
+			// Modding - Add TileId Changes
+			for (int x = 0; x < 3; x++)
+			{
+				for (int y = 0; y < 14; y++)
+				{
+					AddTreeArea_Center(tileEdits, startX + x * 3, startY + y * 3);
+				}
+			}
+
+			// Modding - Add Sidding
+			for (int y = 0; y < 14*3; y++)
+			{
+				AddTreeArea_LeftSide(tileEdits, startX - 1, startY + y);
+				AddSquareNoSpawn(Game1.locations[1], startX - 1, startY + y);
+			}
+
+			// Modding - Add Fine Tuning
+			AddTreeArea_LeftDownTurn(tileEdits, 68, 115);
+			AddSquareNoSpawn(Game1.locations[1], 68, 115);
+			//--------------------------------------------------------------------------
+
+			// Finalize Edits
+			//PatchMap(Game1.locations[1], tileEdits);
+
+			return tileEdits;
+			*/
+		}
+		private static void AddSector2()
+		{
+			MapEditor.TreeArea.FillSquareArea(52, 80, 4, 11, true, true, true, true, true, true, true, true);
+		}
+
+		private static void AddSector3()
+		{
+			MapEditor.TreeArea.FillSquareArea(43, 70, 5, 1, false, false, false, false, false, false, false, false);
+		}
+		private static void AddSector4()
+		{
+			MapEditor.TreeArea.FillSquareArea(34, 61, 3, 3, false, false, false, false, false, false, false, false);
+		}
+
+
+	}
+}
+ 
